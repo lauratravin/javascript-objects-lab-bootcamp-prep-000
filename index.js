d621e826a27bc3b7fc5e07b71c54264fec003f29
Laura Travin <laura.travin@gmail.com>
2017-11-07 16:26:15 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,8 +0,0 @@
-var object = { property : 'somevalue'};
-
-
-function updateObjectWithKeyAndValue(object, key, value){
-     return Object.assign({}, object, {[key]: value});
-}
-
-object = updateObjectWithKeyAndValue(object, property2, somevalue2);
