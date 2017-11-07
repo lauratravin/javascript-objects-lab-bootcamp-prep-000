956881501ee07f06b1d2f04b55e9fc4380665f92
Laura Travin <laura.travin@gmail.com>
2017-11-07 18:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  objectNew = Object.assign ({},object};
+  objectNew = Object.assign (object};
   delete objectNew[key];
   return objectNew;
 }
