1e19264272ca4b97b838f832a1fc08201a8590e7
Laura Travin <laura.travin@gmail.com>
2017-11-07 17:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
      return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 }
 
